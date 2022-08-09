818b420c7c804007408bc26b722570b05feee9dc
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-08-09 20:48:23 -0300

    Alterando a lógica para se calcular a nota

--- src/Soma.java
+++ src/Soma.java
@@ -2,15 +2,20 @@
 public class Soma {
 
 	public static void main(String[] args) {
-		
-		
+
+
 		double notaProva1 = 8;
 		double notaProva2 = 4.5;
 		double notaProva3 = 6;
-		
+
 		double media = (notaProva1 + notaProva2 + notaProva3) / 3;
-		
-		System.out.println(Math.ceil(media));
+
+		if (media < 7) {
+			media = Math.ceil(media);
+			System.out.println(Math.ceil(media));
+		} else {
+			System.out.println(media);
+		}
 	}
 
 }
