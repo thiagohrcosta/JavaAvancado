9d6f6dbb878ed5c606a1cf0a5ae4d948f23ec9cd
Thiago Costa <thiagohrcosta86@gmail.com>
2022-08-09 20:58:25 -0300

    Adicionando novas condições e mensagens

--- src/Soma.java
+++ src/Soma.java
@@ -2,19 +2,23 @@
 public class Soma {
 
 	public static void main(String[] args) {
-
-
 		double notaProva1 = 8;
 		double notaProva2 = 4.5;
 		double notaProva3 = 6;
 
 		double media = (notaProva1 + notaProva2 + notaProva3) / 3;
 
-		if (media < 7) {
+		if (media > 6.5 && media < 7) {
 			media = Math.ceil(media);
 			System.out.println(Math.ceil(media));
-		} else {
+			System.out.println("Aluno passou com nota arredondada")
+		} else if (media > 7.1 && media <= 9{
 			System.out.println(media);
+			System.out.println("Aluno passou com boa nota")
+		} else if (media > 9) {
+			System.out.println("Aluno passou com excelência")
+		} else {
+			System.out.println("Aluno foi reprovado")
 		}
 	}
 
