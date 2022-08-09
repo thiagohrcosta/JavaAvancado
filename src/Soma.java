5fb26077ce9566b5154c2fb37f0f17bbae1623fe
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-08-09 20:37:12 -0300

    Projeto criado

---
+++ src/Soma.java
@@ -0,0 +1,16 @@
+
+public class Soma {
+
+	public static void main(String[] args) {
+		
+		
+		double notaProva1 = 8;
+		double notaProva2 = 4.5;
+		double notaProva3 = 6;
+		
+		double media = (notaProva1 + notaProva2 + notaProva3) / 3;
+		
+		System.out.println(Math.ceil(media));
+	}
+
+}
