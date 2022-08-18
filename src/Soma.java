4acf8686d0b6f37b168e2de84089af9a61f83b4e
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-08-18 14:15:55 -0300

    Corrigindo bugs no arquivo Soma

--- src/Soma.java
+++ src/Soma.java
@@ -3,10 +3,10 @@ public class Soma {
 
 	public static void main(String[] args) {
 		double notaProva1 = 8;
-		double notaProva2 = 4.5;
+		double notaProva2 = 10;
 		double notaProva3 = 6;
 		double notaProva4 = 7;
-		double notaProva5 = 10
+		double notaProva5 = 10;
 
 		double media = (notaProva1 + notaProva2 + notaProva3 + notaProva4 + notaProva5) / 5;
 
@@ -16,17 +16,17 @@ public class Soma {
 		if (media > 6.5 && media < 7) {
 			media = Math.ceil(media);
 			System.out.println(Math.ceil(media));
-			System.out.println("Aluno passou com nota arredondada")
-		} else if (media > 7.1 && media <= 9{
+			System.out.println("Aluno passou com nota arredondada");
+		} else if (media > 7.1 && media <= 9){
 			System.out.println(media);
-			System.out.println("Aluno passou com boa nota")
+			System.out.println("Aluno passou com boa nota");
 		} else if (media > 9) {
-			System.out.println("Aluno passou com excelência")
+			System.out.println("Aluno passou com excelência");
 		} else if (media < 6.5 && trabalhoExtra == true) {
-			media = media + pontoExtra
-			System.out.println("Nota do aluno com ponto extra :" + media)
+			media = media + pontoExtra;
+			System.out.println("Nota do aluno com ponto extra :" + media);
 		} else {
-			System.out.println("Aluno foi reprovado")
+			System.out.println("Aluno foi reprovado");
 		}
 	}
 
